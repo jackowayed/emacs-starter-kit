42eec176e181f9ef87369d29589df6158366c924
Daniel Jackoway <danjdel@gmail.com>
2010-06-12 00:20:26 -0400

    add defunkt's vendor method

--- init.el
+++ init.el
@@ -74,3 +74,19 @@
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
 ;;; init.el ends here
+
+
+; for loading libraries in from the vendor directory
+; shamelessly stolen from http://github.com/defunkt/emacs
+(defun vendor (library)
+  (let* ((file (symbol-name library))
+         (normal (concat "~/.emacs.d/vendor/" file))
+         (suffix (concat normal ".el"))
+         (defunkt (concat "~/.emacs.d/defunkt/" file)))
+    (cond
+     ((file-directory-p normal) (add-to-list 'load-path normal) (require library))
+     ((file-directory-p suffix) (add-to-list 'load-path suffix) (require library))
+     ((file-exists-p suffix) (require library)))
+    (when (file-exists-p (concat defunkt ".el"))
+      (load defunkt))))
+(add-to-list 'load-path "~/.emacs.d/vendor")
